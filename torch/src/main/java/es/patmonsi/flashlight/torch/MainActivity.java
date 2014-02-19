7561112e71d36e9d533f6f3c25c5ff26e18e0302
Patrick <patmonsi@gmail.com>
2014-02-20 00:26:07 +0100

    Now its working.

--- torch/src/main/java/es/patmonsi/flashlight/torch/MainActivity.java
+++ torch/src/main/java/es/patmonsi/flashlight/torch/MainActivity.java
@@ -1,65 +1,336 @@
 package es.patmonsi.flashlight.torch;
 
+
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.WindowManager;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainActivity extends Activity {
+/*
+ * Torch is an LED flashlight.
+ */
 
+public class MainActivity extends Activity implements /*Eula.OnEulaAgreedTo,*/ SurfaceHolder.Callback {
+    private ToggleButton toggleButton;
+    private final Context context = this;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String WAKE_LOCK_TAG = "TORCH_WAKE_LOCK";
+
+    private static final int COLOR_DARK = 0xCC000000;
+    private static final int COLOR_LIGHT = 0xCCBFBFBF;
+    private static final int COLOR_WHITE = 0xFFFFFFFF;
+
+    private Camera mCamera;
+    private boolean lightOn;
+    private boolean previewOn;
+    //   private boolean eulaAgreed;
+    private View button;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+
+    private WakeLock wakeLock;
+
+    private static MainActivity torch;
+
+    public MainActivity() {
+        super();
+        torch = this;
+    }
+
+    public static MainActivity getTorch() {
+        return torch;
+    }
+
+    private void getCamera() {
+        if (mCamera == null) {
+            try {
+                mCamera = Camera.open();
+            } catch (RuntimeException e) {
+                Log.e(TAG, "Camera.open() failed: " + e.getMessage());
+            }
+        }
+    }
+
+    /*
+     * Called by the view (see main.xml)
+     */
+
+    public void toggleLight(View view) {
+        toggleLight();
+    }
+
+    private void toggleLight() {
+        if (lightOn) {
+            turnLightOff();
+        } else {
+            turnLightOn();
+        }
+    }
+
+    private void turnLightOn() {
+   /*     if (!eulaAgreed) {
+            return;
+        }*/
+        if (mCamera == null) {
+            Toast.makeText(this, "Camera not found", Toast.LENGTH_LONG);
+            // Use the screen as a flashlight (next best thing)
+            button.setBackgroundColor(COLOR_WHITE);
+            return;
+        }
+        lightOn = true;
+        Parameters parameters = mCamera.getParameters();
+        if (parameters == null) {
+            // Use the screen as a flashlight (next best thing)
+            button.setBackgroundColor(COLOR_WHITE);
+            return;
+        }
+        List<String> flashModes = parameters.getSupportedFlashModes();
+        // Check if camera flash exists
+        if (flashModes == null) {
+            // Use the screen as a flashlight (next best thing)
+            button.setBackgroundColor(COLOR_WHITE);
+            return;
+        }
+        String flashMode = parameters.getFlashMode();
+        Log.i(TAG, "Flash mode: " + flashMode);
+        Log.i(TAG, "Flash modes: " + flashModes);
+        if (!Parameters.FLASH_MODE_TORCH.equals(flashMode)) {
+            // Turn on the flash
+            if (flashModes.contains(Parameters.FLASH_MODE_TORCH)) {
+                parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+                mCamera.setParameters(parameters);
+                button.setBackgroundColor(COLOR_LIGHT);
+                startWakeLock();
+            } else {
+                Toast.makeText(this, "Flash mode (torch) not supported",
+                        Toast.LENGTH_LONG);
+                // Use the screen as a flashlight (next best thing)
+                button.setBackgroundColor(COLOR_WHITE);
+                Log.e(TAG, "FLASH_MODE_TORCH not supported");
+            }
+        }
+    }
+
+    private void turnLightOff() {
+        if (lightOn) {
+            // set the background to dark
+            button.setBackgroundColor(COLOR_DARK);
+            lightOn = false;
+            if (mCamera == null) {
+                return;
+            }
+            Parameters parameters = mCamera.getParameters();
+            if (parameters == null) {
+                return;
+            }
+            List<String> flashModes = parameters.getSupportedFlashModes();
+            String flashMode = parameters.getFlashMode();
+            // Check if camera flash exists
+            if (flashModes == null) {
+                return;
+            }
+            Log.i(TAG, "Flash mode: " + flashMode);
+            Log.i(TAG, "Flash modes: " + flashModes);
+            if (!Parameters.FLASH_MODE_OFF.equals(flashMode)) {
+                // Turn off the flash
+                if (flashModes.contains(Parameters.FLASH_MODE_OFF)) {
+                    parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+                    mCamera.setParameters(parameters);
+                    stopWakeLock();
+                } else {
+                    Log.e(TAG, "FLASH_MODE_OFF not supported");
+                }
+            }
+        }
+    }
+
+    private void startPreview() {
+        if (!previewOn && mCamera != null) {
+            mCamera.startPreview();
+            previewOn = true;
+        }
+    }
+
+    private void stopPreview() {
+        if (previewOn && mCamera != null) {
+            mCamera.stopPreview();
+            previewOn = false;
+        }
+    }
+
+    private void startWakeLock() {
+        if (wakeLock == null) {
+            Log.d(TAG, "wakeLock is null, getting a new WakeLock");
+            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            Log.d(TAG, "PowerManager acquired");
+            wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, WAKE_LOCK_TAG);
+            Log.d(TAG, "WakeLock set");
+        }
+        wakeLock.acquire();
+        Log.d(TAG, "WakeLock acquired");
+    }
+
+    private void stopWakeLock() {
+        if (wakeLock != null) {
+            wakeLock.release();
+            Log.d(TAG, "WakeLock released");
+        }
+    }
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    /*    if (Eula.show(this)) {
+            eulaAgreed = true;
+        }*/
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        surfaceView = (SurfaceView) this.findViewById(R.id.surfaceview);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        disablePhoneSleep();
+        Log.i(TAG, "onCreate");
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        toggleButton = (ToggleButton) findViewById(R.id.togglebutton);
+
+
+        turnLightOn();
+    }
+
+    public void onToggleClicked(View view) {
+        PackageManager pm=context.getPackageManager();
+        final Parameters p = mCamera.getParameters();
+
+        boolean on = ((ToggleButton) view).isChecked();
+        if (on) {
+            Log.i("info", "torch is turn on!");
+            p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            mCamera.setParameters(p);
+            mCamera.startPreview();
+        } else {
+            Log.i("info", "torch is turn off!");
+            p.setFlashMode(Parameters.FLASH_MODE_OFF);
+            mCamera.setParameters(p);
+            mCamera.stopPreview();
+
         }
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    private void disablePhoneSleep() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+    public void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+        getCamera();
+        startPreview();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        turnLightOn();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        turnLightOff();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mCamera != null) {
+            stopPreview();
+            mCamera.release();
+            mCamera = null;
+        };
+        torch = null;
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mCamera != null) {
+            turnLightOff();
+            stopPreview();
+            mCamera.release();
+        }
+        Log.i(TAG, "onDestroy");
+    }
+
+   /*
+    @Override
+    public void onEulaAgreedTo() {
+        Log.d(TAG, "onEulaAgreedTo");
+        eulaAgreed = true;
+    //    turnLightOn();
+    }
+    */
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        // When the search button is long pressed, quit
+        if (keyCode == KeyEvent.KEYCODE_SEARCH) {
+            finish();
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int I, int J, int K) {
+        Log.d(TAG, "surfaceChanged");
+    }
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceCreated");
+        try {
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceDestroyed");
+    }
 }
+
+
+
